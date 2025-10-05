549a12e17087e91cefca125dc21c6aae2608cb89
Sven-27 <github.geometric664@passinbox.com>
2025-10-05 13:34:03 +0200

    added: colored border when surtain conditions are met.

--- js/script.js
+++ js/script.js
@@ -9,7 +9,28 @@ const TIP_AMOUNT = document.getElementById("tip-amount");
 const TOTAL = document.getElementById("total");
 const RESET_BUTTON = document.getElementById("reset-button");
 
-BILL.addEventListener("keyup", function(event){
 
-  console.log(BILL.value);
-})
+// initial values
+let tipPercentage = 0;
+let billAmount = 0;
+let numberOfPeople = 1;
+
+// add green border to bill input when input is focused and has a value greater than 0
+BILL.addEventListener("input", function() {
+    if(BILL.value <= 0 || !BILL.value) {
+        BILL_CONTAINER.classList.remove("focus");
+    } else {
+        BILL_CONTAINER.classList.add("focus");
+    }
+});
+
+// add orange border to people input when input is has a value of 0 and show error message
+PEOPLE.addEventListener("input", function() {
+    if(PEOPLE.value <= 0 || !PEOPLE.value) {
+        PEOPLE_CONTAINER.classList.add("error");
+        ERROR_MESSAGE.style.display = "flex";
+    } else {
+        PEOPLE_CONTAINER.classList.remove("error");
+        ERROR_MESSAGE.style.display = "none";
+    }
+  });
